0fbaecdc0690da4f0c8e05a97b5000cc0ee27a44
survivor999 <survivor999@hotmail.com>
2017-10-20 18:06:54 +0000

    testing files saved.

---
+++ cmd.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+git init
+git clone https://github.com/survivor999/codingground.git
+
+# git  add  cmd.sh tracks this file so it will be
+# added to the repository.
+#  git status
+#  git add cmd,sh a  second time smtages file to be committed
+#  must do git add cmd.sh each time you change it.
